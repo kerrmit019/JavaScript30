d252b2219484ba5e8b1611bdee8de38ccc194ff9
Sarah Kerr <sarah.kerr2@hotmail.com>
2022-01-04 14:13:29 +1300

    Complete JS drumkit

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,9 +1,32 @@
-// select all the keys and change the border by toggling "playing" class on clicko
-// this isn't what we need to do for the project but proves it toggles the change
-const keys = document.querySelectorAll(".key");
-keys.forEach((key) => {
-  key.addEventListener("click", () => key.classList.toggle("playing"));
-});
+window.addEventListener("keydown", playSound);
 
-// TODO Link specific key press to playing sound
-// TODO Link key press/playing sound to toggle of css change (border etc)
+//  Link key press/playing sound to toggle of css change (border etc)
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
+}
+
+//  Link specific key press to playing sound
+//
+// <audio data-key="65" src="sounds/clap.wav"></audio>
+
+//   <div data-key="75" class="key"></div>
+
+function playSound(event) {
+  console.log(event.keyCode);
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+  if (!audio) {
+    return;
+  }
+  console.log(audio);
+  key.classList.add("playing");
+  // add reset to 0 so can play multiple of same sound in a row
+  //    without waitihg to finish
+  audio.currentTime = 0;
+  audio.play();
+}
